9ff93a18587e791fecea95cb2309f49294f4f195
rusty-sediment <sedimentary@disroot.org>
2023-05-30 22:30:19 -0500

    Added first benchmark

---
+++ benches/filter_benchmark.rs
@@ -0,0 +1,33 @@
+use std::ptr::null_mut;
+use windows_sys::Win32::Foundation::UNICODE_STRING;
+use criterion::{Criterion, criterion_group, criterion_main, black_box};
+
+use sediment_rs::PasswordFilter;
+macro_rules! create_unicode {
+    ( $data:expr ) => {
+        {
+            let mut data = String::from($data)
+                                        .encode_utf16()
+                                        .collect::<Vec<u16>>();
+
+            UNICODE_STRING {
+                Length: (data.len() * 2) as u16,
+                MaximumLength: (data.capacity() * 2) as u16,
+                Buffer: data.as_mut_ptr()
+            }
+        }
+    }
+}
+
+pub fn filter_bench(c: &mut Criterion) {
+    c.bench_function(
+        "pass_filter",
+        |bencher| {
+            let mut password = create_unicode!(black_box("car1234"));
+            bencher.iter( || unsafe{ PasswordFilter(null_mut(), null_mut(), &mut password, 0) })
+        }
+    );
+}
+
+criterion_group!(benches, filter_bench);
+criterion_main!(benches);
