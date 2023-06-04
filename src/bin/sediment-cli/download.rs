511ebcb86c9ffe0c58a927dda883711dae72abe5
rusty-sediment <sedimentary@disroot.org>
2023-06-04 16:35:50 -0500

    Reworked downloads to solve potential issues Now writing to temp files in case anything panics Renaming temp files when done to prevent copying

--- src/bin/sediment-cli/download.rs
+++ src/bin/sediment-cli/download.rs
@@ -1,8 +1,8 @@
-use std::{fs::{File, OpenOptions, read_dir, remove_file}, time::Duration, num::NonZeroU32, sync::{Arc, atomic::{AtomicBool, Ordering}}, collections::{HashMap, hash_map::DefaultHasher}, io::{Read, Write}, path::PathBuf, hash::{Hash, Hasher}};
+use std::{fs::{File, read_dir, rename}, time::Duration, num::NonZeroU32, sync::{Arc, atomic::{AtomicBool, Ordering}}, collections::{HashMap, hash_map::DefaultHasher}, io::{Read, Write}, path::PathBuf, hash::{Hash, Hasher}};
 use governor::{RateLimiter, Quota, Jitter, state::{NotKeyed, InMemoryState}, clock::{QuantaClock, QuantaInstant}, middleware::NoOpMiddleware};
+use flate2::{write::GzEncoder, read::GzDecoder, Compression};
 use winreg::{RegKey, enums::HKEY_LOCAL_MACHINE};
 use indicatif::{ProgressBar, ProgressStyle};
-use flate2::{write::GzEncoder, read::GzDecoder, Compression};
 use tokio::sync::mpsc::{self, Sender};
 use reqwest::StatusCode;
 use xorf::BinaryFuse8;
@@ -106,7 +106,8 @@ pub async fn main(ctrlc_handler: Arc<AtomicBool>, download_path: Option<PathBuf>
     let client = reqwest::Client::new();
     let progress_bar = ProgressBar::new(0xFFFFFu64).with_style(ProgressStyle::with_template("[{elapsed_precise}] {msg} {wide_bar} {human_pos}/{human_len}").unwrap());
 
-    let app_path: String = {
+    // Retrieve app and download path from registry
+    let (app_path, dl_path) = {
         let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
         let app_key = match hklm.create_subkey("SOFTWARE\\sediment") {
             Ok(key) => key,
@@ -116,13 +117,26 @@ pub async fn main(ctrlc_handler: Arc<AtomicBool>, download_path: Option<PathBuf>
             }
         };
 
-        match app_key.get_value("Path") {
+        let app: String = match app_key.get_value("Path") {
             Ok(path) => path,
             Err(_) => {
                 progress_bar.abandon_with_message("Could not find 'Path' key. Is the installation corrupt?");
                 return;
             }
-        }
+        };
+
+        let download: String = match download_path {
+            Some(path) => path.to_string_lossy().to_string(),
+            None => match app_key.get_value("DownloadPath") {
+                Ok(path) => path,
+                Err(_) => {
+                    progress_bar.abandon_with_message("Could not find 'DownloadPath' key. Is the installation corrupt?");
+                    return;
+                }
+            }
+        };
+
+        (app, download)
     };
 
     let download_state = match sled::open(format!("{app_path}\\state")) {
@@ -133,6 +147,7 @@ pub async fn main(ctrlc_handler: Arc<AtomicBool>, download_path: Option<PathBuf>
         }
     };
     
+    // Set up environment for downloading
     let (tx, mut rx) = mpsc::channel(256);
     let limiter = Arc::new(RateLimiter::direct(Quota::per_second(NonZeroU32::new(384).unwrap())));
     let progress_clone = progress_bar.clone();
@@ -145,34 +160,24 @@ pub async fn main(ctrlc_handler: Arc<AtomicBool>, download_path: Option<PathBuf>
     
     drop(tx);
     let mut range_streams: HashMap<String, GzEncoder<File>> = HashMap::new();
+    let mut range_states: HashMap<String, Vec<String>> = HashMap::new();
+
+    // Read all download results as they come through
     while let Some(res) = rx.recv().await {
         match res {
             Ok((range, etag, content)) => {
                 let range_name = range[..2].to_owned();
-                let mut filepath = range_name.clone();
+                let filepath = format!("{dl_path}\\{range_name}.temp");
 
-                let download_path = download_path.clone();
-                if download_path.is_some() {
-                    filepath.insert_str(0, download_path.unwrap().to_str().unwrap());
-                } else {
-                    filepath.insert_str(0, format!("{app_path}\\downloads\\").as_str());
-                }
-
-                let stream = range_streams.entry(range_name).or_insert_with(move || {
-                    let file = OpenOptions::new()
-                                        .append(true)
-                                        .create(true)
-                                        .open(filepath).expect("Failed to open file");
-
-                    GzEncoder::new(file, Compression::new(9))
-                });
+                let stream = range_streams.entry(range_name.clone())
+                    .or_insert(GzEncoder::new(File::create(filepath).expect("Failed to open file"), Compression::new(9)));
                 
                 for line in content.lines() {
                     let hash = range.clone() + line.split(':').next().unwrap() + "\r\n";
                     stream.write_all(hash.as_bytes()).expect("Failed to write hash into file");
                 }
 
-                download_state.insert(range, etag.as_bytes()).expect("Failed to insert into state db");
+                range_states.entry(range_name).or_insert(Vec::new()).push(etag);
                 progress_bar.inc(1);
             },
             Err(err) => {
@@ -182,33 +187,47 @@ pub async fn main(ctrlc_handler: Arc<AtomicBool>, download_path: Option<PathBuf>
     }
 
     progress_bar.set_message("Finishing streams...");
-    for stream in range_streams.into_values() {
+    for (range, stream) in range_streams.into_iter() {
+        // Finish the Gzip stream and drop the underlying File
         match stream.finish() {
-            Ok(_) => {},
+            Ok(writer) => drop(writer),
             Err(err) => {
                 println!("Encountered an error closing a stream: {err:?}");
+                continue;
             }
+        }
+
+        // Replace previous downloaded range with results from tempfile
+        match rename(format!("{dl_path}\\{range}.temp"), format!("{dl_path}\\{range}")) {
+            Ok(_) => {
+                // On successful move, save download state of all ranges involved
+                for etag in range_states.remove(&range).expect("Encountered an error retrieving expected range states") {
+                    download_state.insert(range.clone(), etag.as_bytes()).expect("Failed to insert into state db");
+                }
+            },
+            Err(err) => println!("Encountered an error overwriting destination file: {err:?}")
         }
     }
 
-    let filter_path = filter_path.unwrap_or({
-        let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
-        // Safe to unwrap if first call was successful
-        let app_key = hklm.create_subkey("SOFTWARE\\sediment").unwrap();
-
-        let path: String = match app_key.get_value("FilterPath") {
-            Ok(path) => path,
-            Err(_) => {
-                progress_bar.abandon_with_message("Could not find 'FilterPath' key. Is the installation corrupt?");
-                return;
+    let filter_path: String = match filter_path {
+        Some(path) => path.to_string_lossy().to_string(),
+        None => {
+            let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
+            // Safe to unwrap if first call was successful
+            let app_key = hklm.create_subkey("SOFTWARE\\sediment").unwrap();
+    
+            match app_key.get_value("FilterPath") {
+                Ok(path) => path,
+                Err(_) => {
+                    progress_bar.abandon_with_message("Could not find 'FilterPath' key. Is the installation corrupt?");
+                    return;
+                }
             }
-        };
-
-        path.into()
-    });
+        }
+    };
     
     progress_bar.set_message("Converting hashes into filter structure...");
-    match read_dir(download_path.unwrap_or(format!("{app_path}\\downloads").into())) {
+    match read_dir(dl_path) {
         Ok(dir_iter) => {
             for entry in dir_iter {
                 let entry = match entry {
@@ -219,81 +238,84 @@ pub async fn main(ctrlc_handler: Arc<AtomicBool>, download_path: Option<PathBuf>
                     }
                 };
 
-                let file = match File::open(entry.path()) {
-                    Ok(file) => file,
-                    Err(err) => {
-                        println!("Failed to open hash file: {err:?}");
-                        continue;
-                    }
-                };
+                let path = entry.path();
+                if !path.ends_with(".temp") {
+                    let hashes = {
+                        let file = match File::open(path.clone()) {
+                            Ok(file) => file,
+                            Err(err) => {
+                                println!("Failed to open hash file: {err:?}");
+                                continue;
+                            }
+                        };
 
-                let mut decoder = GzDecoder::new(file);
-                let mut hashes = String::new();
-                match decoder.read_to_string(&mut hashes) {
-                    Ok(_) => {},
-                    Err(err) => {
-                        println!("Failed to read hash file: {err:?}");
-                        continue;
-                    }
-                }
+                        let mut decoder = GzDecoder::new(file);
+                        let mut hashes = String::new();
+                        match decoder.read_to_string(&mut hashes) {
+                            Ok(_) => {},
+                            Err(err) => {
+                                println!("Failed to read hash file: {err:?}");
+                                continue;
+                            }
+                        }
 
-                let hashes_array = hashes.lines()
-                                            .map(|h| {
-                                                let mut hasher = DefaultHasher::new();
-                                                h.trim().to_string().hash(&mut hasher);
-                                                hasher.finish()
-                                            })
-                                            .collect::<Vec<u64>>();
-
-                let filter = loop {
-                    match BinaryFuse8::try_from(&hashes_array) {
-                        Ok(filter) => break filter,
-                        Err(_) => {
-                            progress_bar.set_message("Failed to create filter, trying again...");
+                        hashes
+                    };
+    
+                    let hashes_array = hashes.lines()
+                        .map(|h| {
+                            let mut hasher = DefaultHasher::new();
+                            h.trim().hash(&mut hasher);
+                            hasher.finish()
+                        })
+                        .collect::<Vec<u64>>();
+    
+                    let filter = loop {
+                        match BinaryFuse8::try_from(&hashes_array) {
+                            Ok(filter) => break filter,
+                            Err(_) => {
+                                progress_bar.set_message("Failed to create filter, trying again...");
+                                continue;
+                            }
+                        }
+                    };
+    
+                    let file_name = path.file_name().unwrap().to_string_lossy().to_string();
+                    // Scope to drop `output_file` when finished writing
+                    {
+                        let mut output_file = match File::create(format!("{filter_path}\\{file_name}.temp")) {
+                            Ok(file) => file,
+                            Err(err) => {
+                                println!("Failed to create temp hash filter file: {err:?}");
+                                continue;
+                            }
+                        };
+        
+                        let metadata_results = vec![
+                            output_file.write_all(&filter.seed.to_le_bytes()),
+                            output_file.write_all(&filter.segment_length.to_le_bytes()),
+                            output_file.write_all(&filter.segment_length_mask.to_le_bytes()),
+                            output_file.write_all(&filter.segment_count_length.to_le_bytes())
+                        ];
+        
+                        if metadata_results.into_iter().any(|res| res.is_err()) {
+                            println!("Failed to write filter metadata");
                             continue;
                         }
-                    }
-                };
-
-                let file_path = format!("{}\\{}", filter_path.display(), entry.file_name().to_str().unwrap());
-                let mut output_file = match File::create(file_path.clone()) {
-                    Ok(file) => file,
-                    Err(err) => {
-                        println!("Failed to create hash filter file: {err:?}");
-                        continue;
-                    }
-                };
-
-                let metadata_results = vec![
-                    output_file.write_all(&filter.seed.to_le_bytes()),
-                    output_file.write_all(&filter.segment_length.to_le_bytes()),
-                    output_file.write_all(&filter.segment_length_mask.to_le_bytes()),
-                    output_file.write_all(&filter.segment_count_length.to_le_bytes())
-                ];
-
-                if metadata_results.into_iter().any(|res| res.is_err()) {
-                    println!("Failed to write filter metadata");
-                    drop(output_file);
-
-                    match remove_file(file_path) {
-                        Ok(_) => {},
-                        Err(err) => println!("Failed to delete corrupt hash filter file: {err:?}")
-                    }
-
-                    continue;
-                }
-
-                let data: Vec<u8> = filter.fingerprints.bytes().flatten().collect();
-                match output_file.write_all(&data) {
-                    Ok(_) => {},
-                    Err(err) => {
-                        println!("Failed to write hash filter fingerprints to the file: {err:?}");
-                        drop(output_file);
-
-                        match remove_file(file_path) {
+        
+                        let data: Vec<u8> = filter.fingerprints.bytes().flatten().collect();
+                        match output_file.write_all(&data) {
                             Ok(_) => {},
-                            Err(err) => println!("Failed to delete corrupt hash filter file: {err:?}")
+                            Err(err) => {
+                                println!("Failed to write hash filter fingerprints to the file: {err:?}");
+                                continue;
+                            }
                         }
+                    }
+
+                    match rename(format!("{filter_path}\\{file_name}.temp"), format!("{filter_path}\\{file_name}")) {
+                        Ok(_) => {},
+                        Err(err) => println!("Failed to replace the hash filter file with the temp file: {err:?}")
                     }
                 }
             }
